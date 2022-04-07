2cb9d38f1d203456182a7d60a36ac7e60b2bf5f5
unknown <stepanus97@yandex.ru>
2022-04-07 20:00:17 +0500

    Some Commit

---
+++ Game/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            var model = new Model();
+            var label = new Label()
+            {
+                Width = 400,
+                Text = "Clicks Count: " + model.Counter
+            };
+            var button = new Button();
+            button.Width = 400;
+            button.Height = 100;
+            button.Top = label.Bottom;
+            button.Text = "Increment";
+            button.Click += (sender, args) =>
+            {
+                model.Increment();
+                Invalidate();
+            };
+            Controls.Add(button);
+            Controls.Add(label);
+            Paint += (sender, args) =>
+            {
+                label.Text = "Clicks Count: " + model.Counter;
+            };
+            InitializeComponent();
+        }
+    }
+
+    public class Model
+    {
+        public int Counter { get; private set; }
+
+        public void Increment()
+        {
+            Counter++;
+        }
+    }
+}
